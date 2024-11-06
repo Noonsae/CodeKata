e1d8bbde46cc39fdd31dc53d51376c18ca335ff7
Noon_sae <minseock7308@gmail.com>
2024-11-06 09:11:46 +0900

    📝16. 문자열을 정수로 바꾸기 문제풀이

---
+++ day_16.js
@@ -0,0 +1,12 @@
+//  16. 문자열을 정수로 바꾸기
+//  https://school.programmers.co.kr/learn/courses/30/lessons/12925
+//  문자열 s를 숫자로 변환한 결과를 반환하는 함수, solution을 완성하세요.
+
+function solution(s){
+  return Number(s);
+}
+
+console.log(solution(1234));
+console.log(typeof solution(1234) );
+console.log(solution(-1234));
+console.log(typeof solution(-1234));
