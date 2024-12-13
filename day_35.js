69421696e67ba65f8278d189236fe6fd1085c785
Noon_sae <minseock7308@gmail.com>
2024-12-13 09:41:06 +0900

    📝 35. 소수 만들기 문제풀이

---
+++ day_35.js
@@ -0,0 +1,31 @@
+//  35. 소수 만들기
+//  https://school.programmers.co.kr/learn/courses/30/lessons/12977
+//  주어진 숫자 중 3개의 수를 더했을 때 소수가 되는 경우의 개수를 구하려고 합니다. 숫자들이 들어있는 배열 nums가 매개변수로 주어질 때, nums에 있는 숫자들 중 서로 다른 3개를 골라 더했을 때 소수가 되는 경우의 개수를 return 하도록 solution 함수를 완성해주세요.
+
+function solution(nums) {
+  let answer = 0;
+
+  // 소수 판별 함수
+  function isPrime(num) {
+    if (num < 2) return false;
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+      if (num % i === 0) return false;
+    }
+    return true;
+  }
+
+  // 세 숫자 조합 합 계산 및 소수 판별
+  for (let i = 0; i < nums.length - 2; i++) {
+    for (let j = i + 1; j < nums.length - 1; j++) {
+      for (let k = j + 1; k < nums.length; k++) {
+        const sum = nums[i] + nums[j] + nums[k];
+        if (isPrime(sum)) answer++;
+      }
+    }
+  }
+
+  return answer;
+}
+
+console.log(solution([1, 2, 3, 4])); // 1
+console.log(solution([1, 2, 7, 6, 4])); // 4
